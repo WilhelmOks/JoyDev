e1d87ed236e501fe2863b5c0547d700871d49b14
Wilhelm Oks <oksw@Mac.fritz.box>
2025-03-08 15:38:04 +0100

    open-api generation

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,27 @@
 import Fluent
 import Vapor
+import VaporToOpenAPI
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "JoyDev Server"
     }
 
     app.get("hello") { req async -> String in
-        "Hello, world!"
+        "Hello, JoyDev!"
     }
 
     try app.register(collection: TodoController())
+    
+    // generate OpenAPI documentation
+    app.get("openapi", "openapi.json") { req in
+        req.application.routes.openAPI(
+            info: InfoObject(
+                title: "JoyDev Server",
+                //description: "Example API description",
+                version: "0.1.0"
+            )
+        )
+    }
+    .excludeFromOpenAPI()
 }
