1575d89b86519b0798d96b44bfd068e31e8b487c
Wilhelm Oks <oksw@Mac.fritz.box>
2025-03-10 11:06:52 +0100

    added sqlite and set as active

--- Package.swift
+++ Package.swift
@@ -9,13 +9,20 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
+        
         // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        
         // 🐘 Fluent driver for Postgres.
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
+        
+        // Fluent driver for SQLite.
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         
+        // OpenAPI generation
         .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "4.8.0"),
     ],
     targets: [
@@ -24,6 +31,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,23 +1,46 @@
 import NIOSSL
 import Fluent
 import FluentPostgresDriver
+import FluentSQLiteDriver
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-
-    app.migrations.add(CreateTodo())
-    // register routes
+    try await configureDB(app)
+    
     try routes(app)
+}
+
+private func configureDB(_ app: Application) async throws {
+    enum Kind {
+        case postgres
+        case sqlite
+        case sqliteInMemory
+    }
+    
+    let kind: Kind = { .sqlite }()
+    
+    switch kind {
+    case .postgres:
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    case .sqlite:
+        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    case .sqliteInMemory:
+        app.databases.use(.sqlite(.memory), as: .sqlite)
+    }
+    
+    app.migrations.add(CreateTodo())
+    
+    if kind != .postgres {
+        try await app.autoMigrate()
+    }
 }
